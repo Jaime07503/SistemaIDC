6b1689be889c3ff1adb0b2bfa0c4dc2fd1247890
Mario Martinez <mariojaimemartinez27@gmail.com>
2023-09-18 16:40:04 -0600

    Agregando autenticacion con Google

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,9 +18,12 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->string('avatar')->nullable();
+            $table->string('external_id')->nullable();
+            $table->string('external_auth')->nullable();
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login-google', function () {
+    return Socialite::driver('google')->redirect();
+});
+ 
+Route::get('/google-callback', function () {
+    $user = Socialite::driver('google')->user();
+
+    $usuarioExiste = User::where('external_id', $user->id)->where('external_auth', 'google')->first();
+    
+    if($usuarioExiste) {
+        Auth::login($usuarioExiste);
+    } else {
+        $usuarioNuevo = User::create([
+            'name' => $user->name,
+            'email' => $user->email,
+            'avatar' => $user->avatar,
+            'external_id' => $user->id,
+            'external_auth' => 'google'
+        ]);
+
+        Auth::login($usuarioNuevo);
+    }
+
+    return redirect('/dashboard');
 });
